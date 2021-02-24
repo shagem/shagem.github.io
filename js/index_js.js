e006e61667d82ce6b0b4b468c08bf19bb71d7cd5
shagem <seanhagemanweb@gmail.com>
2021-02-24 16:01:49 -0500

    update 3

--- js/index_js.js
+++ js/index_js.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
   $(".accordion").hide().fadeIn(3000);
 });
 
+$(document).ready(function(){
+	$("#terrance").click(function(){
+		$("#terrance_img").toggle();
+		});
+});
 
-
-
+$(document).ready(function(){
+	$("#ban_the_scan").click(function(){
+		$("#ban_the_scan_img").toggle();
+		});
+});
