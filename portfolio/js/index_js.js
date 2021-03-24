0b86e5e97b13959d284fe46422066eb9f97d4972
shagem <seanhagemanweb@gmail.com>
2021-03-24 18:05:48 -0400

    update again

--- portfolio/js/index_js.js
+++ portfolio/js/index_js.js
@@ -1,15 +1,33 @@
+
+//fade command
 $(document).ready(function(){
   $(".hidden").hide().fadeIn(3000);
 });
 
+
+
+//Work page js
 $(document).ready(function(){
-	$("#terrance").click(function(){
-		$("#terrance_img").toggle();
+  $("#web_design_content").hide();
+  $("#digital_art_content").hide();
+  $("#photography_content").hide();
+});
+
+$(document).ready(function(){
+	$("#web_design").click(function(){
+		$("#web_design_content").toggle();
 		});
 });
 
 $(document).ready(function(){
-	$("#ban_the_scan").click(function(){
-		$("#ban_the_scan_img").toggle();
+	$("#photography").click(function(){
+		$("#photography_content").toggle();
 		});
 });
+
+$(document).ready(function(){
+	$("#digital_art").click(function(){
+		$("#digital_art_content").toggle();
+		});
+});
+
