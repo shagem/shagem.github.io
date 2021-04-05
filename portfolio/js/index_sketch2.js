8fbd5f9c7c6a372953ffee738fd018fce83cf237
shagem <seanhagemanweb@gmail.com>
2021-04-05 18:26:15 -0400

    update

--- portfolio/js/index_sketch2.js
+++ portfolio/js/index_sketch2.js
@@ -4,27 +4,38 @@ function setup() {
   createCanvas(400, 400, WEBGL);
 }
 
+
+function spinUp() {
+	
+	if (mouseIsPressed) {
+		normalMaterial();
+		stroke(255, 255, 255);
+		rotateX(radians(frameCount));
+		sphere(150, 15, 15);
+	}
+	else
+	{
+		if (mouseX > 200) {
+			normalMaterial();
+			stroke(255, 255, 255);
+			rotateY(radians(frameCount));
+			sphere(150, 15, 15);
+		}
+		else
+		{
+			normalMaterial();
+			stroke(255, 255, 255);
+			rotateY(-radians(frameCount));
+			sphere(150, 15, 15);
+		}
+	}
+}
+
+
 function draw() {
   background(0, 0, 0);
   
-  push();
-  
-  
-  if (mouseIsPressed) {
-	normalMaterial();
-	stroke(255, 255, 255);
-	rotateX(radians(frameCount));
-	sphere(150, 15, 15);
-  }	else {
-	normalMaterial();
-	stroke(255, 255, 255);
-	rotateY(radians(frameCount));
-	sphere(150, 15, 15);
-  }
-  pop();
-  
-  
-  
+	spinUp();
   
 }
 
