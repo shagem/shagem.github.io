7093c75bd469a14c2cda2984bd5f1b5fbda42985
shagem <seanhagemanweb@gmail.com>
2021-05-05 12:09:45 -0400

    update experimental web2

--- experimental_web/js/image_2.js
+++ experimental_web/js/image_2.js
@@ -1,9 +1,4 @@
 
-let myFont;
-
-function preload() {
-	myFont = loadFont('fonts/typographica.ttf');
-}
 
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
@@ -15,25 +10,64 @@ function setup() {
 	
 }
 
-
+let cordX;
+let cordY;
+let borderWidth;
+let shapeWidth;
+let shapeHeight;
+let fr;
+let fg;
+let fb;
+let fo;
+let sr;
+let sg;
+let sb;
 
 function draw() {
 
-	let string = "Click Your Mouse";
-
-	textSize(40);
-	fill(255);
-	textFont(myFont);
-	text(string, windowWidth / 2, windowHeight / 2);
+	cordX = random( -windowWidth, windowWidth);
+	cordY = random( -windowHeight, windowHeight);
+	borderWidth = random( 1, 3 );
+	shapeWidth = random( 10, 150 );
+	shapeHeight = random( 10, 110 );
 	
-		
+	fr = random( 1, 255 );
+	fg = random( 1, 255 );
+	fb = random( 1, 255 );
+	fo = random( 1, 230 );
+	
+	sr = random( 1, 255 );
+	sg = random( 1, 255 );
+	sb = random( 1, 255 );
+	
+	stroke(sr, sg, sb);
+	strokeWeight(borderWidth);
+	fill(fr, fg, fb, fo);
+	
+	if (mouseIsPressed){
+		if(mouseButton === LEFT)
+		{
+			circle(cordX, cordY, shapeWidth);
+		}
+		if(mouseButton === RIGHT)
+		{
+			circle(mouseX - (windowWidth / 2), mouseY - (windowHeight / 2), 10 * borderWidth);
+			rect(cordX, cordY, shapeWidth, shapeHeight);
+		}
+		if(mouseButton === CENTER)
+		{
+			
+		}
+	}
+	else {
+		rect(cordX, cordY, shapeWidth, shapeHeight);
+	}
+	
 }
 
 
 
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+
 
 
