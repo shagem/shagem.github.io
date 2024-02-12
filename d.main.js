96c81421bf149dbe37d361ea96636cd1de52920d
Sean <56557448+shagem@users.noreply.github.com>
2024-02-12 13:11:43 -0500

    Delete d.main.js

--- d.main.js
+++
@@ -1,9 +0,0 @@
-let body = document.querySelector('html');
-
-fetch('https://seanhageman.com/d.main.html')
-.then(response => response.text())
-.then(function(response) {
-    body.innerHTML = response;
-    })
-.catch(err => console.error(err)
-);
