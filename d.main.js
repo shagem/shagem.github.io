512ada3e9181460a806d9ef150bad80faa0c12fd
Sean <56557448+shagem@users.noreply.github.com>
2024-02-12 11:29:55 -0500

    Update d.main.js

--- d.main.js
+++ d.main.js
@@ -4,6 +4,7 @@ async function fetchMyDocument() {
   try {
     let response = await fetch('https://seanhageman.com/d.main.html'); // Gets a promise
     mainContent.innerHTML = await response.text(); // Replaces body with response
+    console.log('Success');
   } catch (err) {
     console.log('Fetch error:' + err); // Error handling
   }
