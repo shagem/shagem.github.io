dca4d1863f54db35ea25bbe34d9fcc6ab8457021
Sean <56557448+shagem@users.noreply.github.com>
2024-02-12 11:32:07 -0500

    Add files via upload

--- d.main.js
+++ d.main.js
@@ -1,11 +1,12 @@
 let mainContent = document.getElementById('maincontent');
 
-async function fetchMyDocument() {      
-  try {
-    let response = await fetch('https://seanhageman.com/d.main.html'); // Gets a promise
-    mainContent.innerHTML = await response.text(); // Replaces body with response
-    console.log('Success');
-  } catch (err) {
-    console.log('Fetch error:' + err); // Error handling
-  }
+function fetchHtml() {
+  fetch('https://seanhageman.com/d.main.html')
+  .then((response) => {
+    return response.text();
+  })
+  .then((html) => {
+    mainContent.innerHTML = html;
+    console.log('Success')!
+  });
 }
