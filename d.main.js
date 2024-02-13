0b635f9ec096c355d06886694640a7231ad82476
Sean <56557448+shagem@users.noreply.github.com>
2024-02-13 10:40:46 -0500

    Add files via upload

--- d.main.js
+++ d.main.js
@@ -1,5 +1,8 @@
 const dropdown = document.getElementById('navDropdown');
 dropdown.addEventListener('click', function(){
-    document.getElementById('navLink').classList.toggle('active');
+    document.getElementById('navProductsLink').classList.toggle('active');
+    document.getElementById('navLoginLink').classList.toggle('active');
+    document.getElementById('navFTPLink').classList.toggle('active');
+    document.getElementById('navContactLink').classList.toggle('active');
     console.log('success!');
 });
