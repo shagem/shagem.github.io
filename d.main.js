21ad618ceac6c73935c129580d9e7225fe7f157f
Sean <56557448+shagem@users.noreply.github.com>
2024-02-13 13:31:55 -0500

    Add files via upload

--- d.main.js
+++ d.main.js
@@ -1,5 +1,12 @@
+
+
+
+
+
+//Mobile Dropdown handler
 const dropdown = document.getElementById('navDropdown');
 dropdown.addEventListener('click', function(){
+    dropdown.classList.toggle('active');
     document.getElementById('navProductsLink').classList.toggle('active');
     document.getElementById('navLoginLink').classList.toggle('active');
     document.getElementById('navFTPLink').classList.toggle('active');
