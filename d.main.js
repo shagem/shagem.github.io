0087c6f68959683cbdb6106065f34b775e402a4a
Sean <56557448+shagem@users.noreply.github.com>
2024-02-14 14:21:49 -0500

    Add files via upload

--- d.main.js
+++ d.main.js
@@ -1,14 +1,12 @@
 // Doyle Rocket Quotes JS, Handles custom events.
 
-
-
-
 //Mobile Dropdown handler
 const dropdown = document.getElementById('navDropdown');
 dropdown.addEventListener('click', function(){
     dropdown.classList.toggle('active');
-    document.getElementById('navProductsLink').classList.toggle('active');
     document.getElementById('navLoginLink').classList.toggle('active');
+    document.getElementById('navProductsLink').classList.toggle('active');
+    document.getElementById('navQuoteLink').classList.toggle('active');
     document.getElementById('navFTPLink').classList.toggle('active');
     document.getElementById('navContactLink').classList.toggle('active');
 });
