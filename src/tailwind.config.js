3cc4920da19e4201c8fe9b3daec98d7f8e967256
Sean <56557448+shagem@users.noreply.github.com>
2024-08-20 10:41:53 -0400

    Update back to Github from WordPress

---
+++ src/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../index.html"
+  ],
+  theme: {
+    colors: {
+      'white': '#fff',
+      'black': '#000',
+      'gray-900': '#212121',
+      'gray-800': '#303030',
+      'gray-700': '#424242',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
